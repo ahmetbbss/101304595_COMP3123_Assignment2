67a084295a5f5f3bb0838ff6df84b58b2a004c9d
ahmetbbss <ahmet.buyukbas@georgebrown.ca>
2023-12-02 13:54:06 -0500

    Employee details  updated

--- src/components/ViewAnEmployee.js
+++ src/components/ViewAnEmployee.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
-function ViewAnEmployee () {
+function ViewAnEmployee() {
+  const { id } = useParams();
+  const [employee, setEmployee] = useState(null);
+  const URL = "http://localhost:8001";
+
+  useEffect(() => {
+    axios
+      .get(`${URL}/api/v1/emp/employees/${id}`)
+      .then((response) => {
+        setEmployee(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching employee data: ", error);
+      });
+  }, [id]);
+
+  if (!employee) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>ViewAnEmployee</div>
-  )
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <Card>
+            <Card.Header>Employee Details</Card.Header>
+
+            <Card.Body>
+              <Card.Title>
+                {employee.first_name} {employee.last_name}
+              </Card.Title>
+
+              <Card.Text>
+                <strong>Email:</strong> {employee.email}
+                <br />
+                <strong>ID:</strong> {employee._id}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default ViewAnEmployee
+export default ViewAnEmployee;
