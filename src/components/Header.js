a0581e15a5ef27184c1bb1580f959661af43feb0
ahmetbbss <ahmet.buyukbas@georgebrown.ca>
2023-12-02 14:50:42 -0500

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 function Header({ username }) {
-  return <header>{username && <p>Logged in as: {username}</p>}</header>;
+  return (
+    <header>
+      {username && (
+        <p style={{ color: 'blue', fontWeight: 'bold', fontSize: '16px' }}>
+          Logged in as: {username}
+        </p>
+      )}
+    </header>
+  );
 }
 
 export default Header;
